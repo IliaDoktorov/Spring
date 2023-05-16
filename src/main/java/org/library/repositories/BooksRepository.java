f0efc16e4611542f96e6ba6f07b1759e4d154367
Ilia Doktorov <whiteoverlord@yandex.ru>
2023-05-16 21:39:17 +0300

    Library app with Hibernate and JPA

--- src/main/java/org/library/repositories/BooksRepository.java
+++
@@ -1,9 +0,0 @@
-package org.library.repositories;
-
-import org.library.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooksRepository extends JpaRepository<Book, Integer> {
-}
