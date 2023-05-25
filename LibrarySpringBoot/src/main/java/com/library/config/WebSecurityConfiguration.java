ea0a7ada84010589935bd04bad2a05832b7d4cf2
Ilia Doktorov <whiteoverlord@yandex.ru>
2023-05-25 18:09:03 +0300

    Custom authentication page with rules

--- LibrarySpringBoot/src/main/java/com/library/config/WebSecurityConfiguration.java
+++ LibrarySpringBoot/src/main/java/com/library/config/WebSecurityConfiguration.java
@@ -4,10 +4,13 @@ import com.library.services.EmployeeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,20 +18,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
-    private EmployeeDetailsService employeeDetailsService;
-
     @Autowired
-    public WebSecurityConfiguration(EmployeeDetailsService employeeDetailsService) {
-        this.employeeDetailsService = employeeDetailsService;
-    }
+    private EmployeeDetailsService employeeDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(requests -> requests
-                .anyRequest().authenticated()
-        ).formLogin(Customizer.withDefaults());
-//        ).formLogin(form -> form
-//                .loginPage("/login").permitAll()
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+        ).formLogin(form -> form
+                .loginPage("/login").permitAll()
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/index", true)
+                .failureUrl("/login?error")
+        );
 //        ).logout(LogoutConfigurer::permitAll);
 
         return http.build();
